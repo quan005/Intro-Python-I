29908b1f22e556df6e7b5361b981fa36a354cd67
DaQuan Freeman <ralphealfreeman@gmail.com>
2020-02-20 18:30:57 -0700

    mvp part II complete

--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -35,13 +35,24 @@ waypoints = [
 
 # Add a new waypoint to the list
 # YOUR CODE HERE
+waypoints.append({
+    "lat": 44,
+    "lon": -123,
+    "name": "a hot place"
+})
 
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
 # Note: It's okay to access the dictionary using bracket notation on the
 # waypoints list.
-
 # YOUR CODE HERE
+waypoints[0].update({
+    "lon": -130,
+    "name": "not a real place"
+})
 
 # Write a loop that prints out all the field values for all the waypoints
 # YOUR CODE HERE
+for obj in waypoints:
+    for i, x in obj.items():
+        print(x)
